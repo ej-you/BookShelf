bbb57970f0eb2d5800a2f4b4bb1bf5fb3e175c7b
Danil-114195722 <ejyou.user@gmail.com>
2025-05-01 22:48:57 +0300

    Refactored main entrypoint

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -9,20 +9,25 @@ import (
 )
 
 func main() {
+	if err := startApp(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func startApp() error {
 	// load config
 	cfg, err := config.New()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
 	// create app
 	application, err := app.New(cfg)
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
-
 	// run app
 	if err := application.Run(); err != nil {
-		log.Fatal(err)
+		return err
 	}
+	return nil
 }
